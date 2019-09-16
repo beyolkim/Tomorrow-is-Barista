20700a67c2d271821535201cef3082b23cc97c28
githubhee <heeheeheeeee@gmail.com>
2019-09-16 10:26:00 +0900

    한번더

--- Tomorro is Barista/Assets/02.Scripts/Move_control.cs
+++ Tomorro is Barista/Assets/02.Scripts/Move_control.cs
@@ -2,32 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move_control : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 
 {
-
-    public GameObject obj1, obj2, obj3;
-    void OnTriggerEnter(Collider other)
-
+    public static int aaaaa;
+    public int bbbbbb ;
+    void Start()
     {
-
-        if (other.gameObject.CompareTag("TEST"))
-
-        {
-                         
-            iTween.MoveTo(obj1, iTween.Hash("z", 1.0f
-                                       , "easeType", "Math.easeOutExpo"
-                                       , "speed", 2.5f
-                                       )); // hashtable 이용해서 사용함.(키, 밸류)
-            //other.gameObject.SetActive(false);
-            Debug.Log("붙었다!");
-
-        }
+        aaaaa = 4;
+        bbbbbb = 3;
     }
 
-
-    // Start is called before the first frame update
-
-
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
-
