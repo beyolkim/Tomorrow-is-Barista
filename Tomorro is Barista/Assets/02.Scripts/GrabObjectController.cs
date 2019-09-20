4391af547abcc962b6aece30c49b46cd5be25333
veroxxx <54518869+veroxxx@users.noreply.github.com>
2019-09-20 12:38:15 +0900

    0920

--- Tomorro is Barista/Assets/02.Scripts/GrabObjectController.cs
+++ Tomorro is Barista/Assets/02.Scripts/GrabObjectController.cs
@@ -9,45 +9,100 @@ public class GrabObjectController : MonoBehaviour
     //입력 소스 정의
     public SteamVR_Input_Sources handType;
     public SteamVR_Behaviour_Pose controllerPose;
-    public SteamVR_Action_Boolean grip; //= SteamVR_Input.GetBooleanAction("GrabGrip");
+    public SteamVR_Action_Boolean grip;
     public SteamVR_Action_Boolean trigger;
 
+    private Transform tr;      // cameraRig아래에 있는 Controller의 위치   
+    
+    private Animator anim;
+    private int hashIsGrabVer; // 애니메이터 파라미터의 해쉬값을 저장할 변수
+    private int hashIsFinger;  // 애니메이터 파라미터의 해쉬값을 저장할 변수
+    
     private GameObject collidingObject; //현재 충돌중인 객체
     private GameObject objectInHand;    //플레이어가 잡은 객체
-
     private FixedJoint fx;
 
     private void Start()
     {
-       fx =  gameObject.GetComponent<FixedJoint>();
+        fx =  gameObject.GetComponent<FixedJoint>();
+
+        //controller의 Transform 추출
+        tr = GetComponent<Transform>();
+
+        //애니메이터 컨트롤러 추출
+        anim = tr.GetComponentInChildren<Animator>(); //첫번째 자식 오브젝트의 animator 컴포넌트 추출 
+        //애니메이터 파라미터의 해시값을 추출해 저장
+        hashIsGrabVer = Animator.StringToHash("GrabVer");
+        hashIsFinger = Animator.StringToHash("Finger");
     }
+
     // Update is called once per frame
     void Update()
     {
         //그랩그립을 누를 때
         if (grip.GetStateDown(handType))
         {
+            GripAnimOn();
+            Debug.Log("난 지금 눌렀어");
+
             if (collidingObject)
             {
                 GrabObject();
             }
         }
 
-        //그랩그립을 뗄 때U
+        //그랩그립을 뗄 때
         if (grip.GetStateUp(handType))
         {
+            GripAnimOff();
+            Debug.Log("난 지금 뗐어");
             if (objectInHand)
             {
                 ReleaseObject();
             }
         }
+
+        //트리거 누를 떄
+        if (trigger.GetStateDown(handType))
+        {
+            FingerAnimOn();
+            Debug.Log("손가락이 접혀요");
+        }
+
+        //트리거 뗄 때
+        if (trigger.GetStateUp(handType))
+        {
+            FingerAnimOff();
+            Debug.Log("손가락이 펴져요");
+        }
+    }
+    public void GripAnimOn()
+    {
+        Debug.Log("주먹을 쥐세요");
+        anim.SetBool(hashIsGrabVer, true); //그립 눌렀을 시 grabver값 true = 잡는 애니메이션 실행해라
     }
 
-    //충돌이 시작되는 순간
-    public void OnTriggerEnter(Collider other)
+    public void GripAnimOff()
     {
+        Debug.Log("주먹을 펴세요");
+        anim.SetBool(hashIsGrabVer, false); //그립 뗐을 시 grabver값 = false = 잡는 애니메이션 꺼라
+    }
+
+    public void FingerAnimOn()
+    {
+        anim.SetBool(hashIsFinger, true);  
+    }
+
+    public void FingerAnimOff()
+    {
+        anim.SetBool(hashIsFinger, false);
+    }
+
+    //콜라이더 충돌이 시작되는 순간
+    public void OnTriggerEnter(Collider other) //충돌감지 콜백함수 
+    {
+        //collidingObject = other.gameObject; 이게 굳이 필요한가?? 왜 써준거지??
         SetCollidingObject(other);
-        collidingObject = other.gameObject;
     }
     //충돌 중
     public void OnTriggerStay(Collider other)
@@ -55,33 +110,35 @@ public class GrabObjectController : MonoBehaviour
         SetCollidingObject(other);
     }
     //충돌이 끝날 때
-    public void OnTriggerExit(Collider other)
+    public void OnTriggerExit(Collider other) //충돌 상태에서 빠져 나왔을 때, 1회 자동 호출
     {
         if (!collidingObject)
         {
             return;
         }
-        collidingObject = null; //null 설정 안하면 어떻게 되는거지?
+        collidingObject = null; 
     }
 
     //충돌중인 객체로 설정
     private void SetCollidingObject(Collider col)
     {
-        if (collidingObject || !col.GetComponent<Rigidbody>())
-        {
-           // return;
+        if (collidingObject || !col.GetComponent<Rigidbody>())  //collidingObject를 2개 잡지 않기 위해 || rigidbody가 없는 object는 충돌객체로 인식x
+        {                                                       
+            return;
         }
 
         collidingObject = col.gameObject;
     }
 
     //객체 잡기
-    private void GrabObject()
+    public void GrabObject()
     {
         objectInHand = collidingObject; //잡은 객체로 설정
         collidingObject = null; //충돌 객체 해제
 
+
         var joint = AddFixedJoint();
+        
         joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
     }
     //FixedJoint = 객체들을 하나로 묶어 고정 시킴
